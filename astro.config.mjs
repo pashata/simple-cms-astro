2ef1845964edbcb470db9e055ff70db5c1df95d9
Dimitar Pashovski <dipa@boozt.com>
2025-06-29 16:14:48 +0200

    add github pages deploy script

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://pashata.github.io',
+    base: '/simple-cms-astro',
+});
