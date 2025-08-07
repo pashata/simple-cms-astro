d0031a4aa9a4d0a0f37875006b5a9de3508ff046
Dimitar Pashovski <epashata@gmail.com>
2025-08-07 23:10:34 +0200

    fix prod config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://pashata.github.io',
-    base: '/simple-cms-astro',
+    site: 'https://masonary.vercel.app',
+    base: '/',
 });
